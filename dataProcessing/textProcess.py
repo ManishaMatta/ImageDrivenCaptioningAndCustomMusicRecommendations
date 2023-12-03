ede0308197f38b7493a60082846fb97248dc5416
MRadhakrishnaSDSU <mradhakrishna9449@sdsu.edu>
2023-12-03 13:08:21 -0800

    added text processing

--- dataProcessing/textProcess.py
+++ dataProcessing/textProcess.py
@@ -1,96 +1,33 @@
-import nltk
 import pandas as pd
 import nltk
-from builtins import *
+import requests
+from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.tag import pos_tag
 from nltk.corpus import wordnet
-import requests
-from bs4 import BeautifulSoup
-import random
-import re
-import os
-from nltk.sentiment import SentimentIntensityAnalyzer
-
-nltk.download('vader_lexicon')
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')  # Ensure wordnet data is downloaded
+nltk.download('wordnet')
 
-url = "https://www.goodreads.com/quotes/tag/free?page="
+# Configuration variables
+CSV_FILE_PATH = 'C:/Users/sushe/Documents/BDA_Fall_2023/Sem1/BDA_696_Python/Project/sample_text.csv'
+BASE_URL1 = "https://www.lifewire.com"
+BASE_URL2 = "https://www.oberlo.com"
+QUOTES_CLASS = "comp mntl-sc-block mntl-sc-block-html"
+#QUOTES_CLASS = "my-12 md:px-24"
+URL1 = f"{BASE_URL1}/best-instagram-captions-4171697"
+URL2 = f"{BASE_URL2}/blog/instagram-captions"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.8",
 }
+quotes = []
 
-
-def get_synonyms(word):
-    synonyms = []
-    for syn in wordnet.synsets(word):
-        for lm in syn.lemmas():
-            synonyms.append(lm.name())
-    return set(synonyms)
-
-
-def fetch_data(url, headers):
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    return soup
-
-
-def get_next_page_url(soup):
-    next_page_url = soup.find('a', class_='next_page')
-    if next_page_url:
-        return next_page_url['href']
-    else:
-        return None
-
-
-def extract_quotes(soup):
-    quotes = []
-    quote_container = soup.find_all('div', class_='quote')
-
-    for quote in quote_container:
-        text = quote.find('div', class_='quoteText').text.strip()
-        quotes.append(text)
-    return quotes
-
-
-def generate_caption(quotes, synonyms):
-    quote = random.choice(quotes)
-    keyword = random.choice(synonyms)
-    caption = f"{quote}"
-    return caption
-
-
-def scrape_quotes_from_whole_website():
-    quotes = []
-    url = "https://www.goodreads.com/quotes/tag/free?page="
-    while True:
-        soup = fetch_data(url, headers)
-        quotes += extract_quotes(soup)
-
-        next_page_url = get_next_page_url(soup)
-        if not next_page_url:
-            break
-        else:
-            url = "https://www.goodreads.com" + next_page_url
-    return quotes
-
-
-def sentiment(line):
-    return SentimentIntensityAnalyzer().polarity_scores(line)
-
-
-CSV_FILE_PATH = 'C:/Users/sushe/Documents/BDA_Fall_2023/Sem1/BDA_696_Python/Project/sample_text.csv'
-BASE_URL = "https://www.lifewire.com"
-QUOTES_CLASS = "comp mntl-sc-block mntl-sc-block-html"
-URL = f"{BASE_URL}/best-instagram-captions-4171697"
-
-
-def scrape_quotes_from_website(url):
-    quotes = []
+def scrape_quotes_from_website1(url):
+    #quotes = []
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
@@ -100,6 +37,38 @@ def scrape_quotes_from_website(url):
             quotes.extend(li.text for li in li_tags)
     return quotes
 
+def scrape_quotes_from_website2(url):
+    #quotes = []
+    response = requests.get(url)
+    response.status_code
+    content = BeautifulSoup(response.text, 'html.parser')
+    for i in content.find_all("li", style="font-weight: 400;"):
+        #print(i.get_text())
+        quotes.append(i.get_text())
+    return quotes
+
+def get_next_page_url(soup):
+    next_page_url = soup.find('a', class_='next_page')
+    if next_page_url:
+        return next_page_url['href']
+    else:
+        return None
+
+def scrape_quotes_from_whole_website():
+    url = "https://www.goodreads.com/quotes/tag/free?page="
+    while True:
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        quote_container = soup.find_all('div', class_='quote')
+        for quote in quote_container:
+            text = quote.find('div', class_='quoteText').text.strip()
+            quotes.append(text)
+        next_page_url = get_next_page_url(soup)
+        if not next_page_url:
+            break
+        else:
+            url = "https://www.goodreads.com" + next_page_url
+    return quotes
 
 def process_and_print_quotes(quotes, text_data):
     for line in text_data:
@@ -121,78 +90,30 @@ def process_and_print_quotes(quotes, text_data):
                 for quote in matching_quotes:
                     print(quote)
                 print('\n')
+    for i in text_data:
+        val = []
+        for j in matching_quotes:
+            vectorizer = CountVectorizer()
+            X = vectorizer.fit_transform([i.strip(), j.strip()])
+            cosine_sim = cosine_similarity(X)[0][1]
+            val.append((cosine_sim, j.strip(), i.strip()))
+        sorted_val_desc = sorted(val, key=lambda x: x[0], reverse=True)
+        print(sorted_val_desc[:3])
 
+def main():
+    txt_df = pd.read_csv(CSV_FILE_PATH, sep='|')
+    txt_dt = txt_df[' comment'].head(1)
+    print(txt_dt)
+    print('\n')
 
-# if __name__ == "__main__":
-#     txt_df = pd.read_csv('C:/Users/sushe/Documents/BDA_Fall_2023/Sem1/BDA_696_Python/Project/results.csv', sep='|')
-#     print(txt_df[' comment'])
-#
-#     synonyms = set()  # Initialize synonyms as a set
-#     for line in txt_df[' comment']:
-#         sentiment = sentiment(line)
-#         if isinstance(line, str):  # Check if it's a string
-#             for ln in sent_tokenize(line):
-#                 wordtokens = word_tokenize(ln)
-#                 tag_word = pos_tag(wordtokens)
-#                 filtered_words = list(filter(lambda word: word[1].startswith('VB') or word[1].startswith('NN') or word[1].startswith('JJ'), tag_word))
-#                 words = [item[0] for item in filtered_words]
-#                 print(' '.join(words) + ":")
-#                 print("\n")
-#                 quotes = scrape_quotes_from_whole_website()
-#                 keyword_list = words
-#                 result = [any(k in q for k in keyword_list) for q in quotes]
-#                 for res, quote in zip(result, quotes):
-#                     if res:
-#                         print(quote)
+    quotes = scrape_quotes_from_website1(URL1)#scrape from lifewire website
+    quotes = scrape_quotes_from_website2(URL2)#scrape from oberlo website
+    quotes = scrape_quotes_from_whole_website()
 
-# def main():
-#     txt_df = pd.read_csv(CSV_FILE_PATH, sep='|')
-#     txt_dt = txt_df[' comment'].head(10)
-#     print(txt_dt)
-#     print('\n')
-#
-#     quotes = scrape_quotes_from_website(URL)
-#     print("Quotes scraped:", len(quotes))  # Add this to check if quotes were scraped
-#     print('\n')
-#
-#     process_and_print_quotes(quotes, txt_dt)
-#
-# if _name_ == "_main_":
-#     main()
+    print("Quotes scraped:", len(quotes))  # Add this to check if quotes were scraped
+    print('\n')
 
-project_name = 'ImageDrivenCaptioningAndCustomMusicRecommendations'
-absolute_path = os.path.dirname(__file__)
-relative_path = "resources/datasets"
-base_path = absolute_path.split(project_name)[0] + project_name
-full_path = os.path.join(base_path, relative_path)
-print(full_path)
+    process_and_print_quotes(quotes, txt_dt)
 
-txt_df = pd.read_csv(full_path + '/sample_textprocess_file.csv', sep='|')
-print(txt_df[' comment'])
-
-#
-# synonyms = set()  # Initialize synonyms as a set
-# quotes = scrape_quotes_from_whole_website()
-#
-# for line in txt_df[' comment']:
-#     if isinstance(line, str):  # Check if it's a string
-#         for ln in sent_tokenize(line):
-#             wordtokens = word_tokenize(ln)
-#             tag_word = pos_tag(wordtokens)
-#             filtered_words = list(filter(lambda word: word[1].startswith('VB') or word[1].startswith('NN') or word[1].startswith('JJ'), tag_word))
-#             words = [item[0] for item in filtered_words]
-#             print(' '.join(words) + ":")
-#             print("\n")
-#             keyword_list = words
-#             result = [any(k in q for k in keyword_list) for q in quotes]
-#             print(result)
-#             # for res, quote in zip(result, quotes):
-#             #     if res:
-#             #         print(quote)
-
-'''result = [all(k in q for k in keyword_list) for q in quotes]
-for res, str in zip(result, quotes):
-    if res:
-        print(str)
-    else:
-        print("No Quotes Available")'''
+if __name__ == "__main__":
+    main()
