b0011d8d3a71601a2e5e96a82c04b8b7038a37fa
MRadhakrishnaSDSU <mradhakrishna9449@sdsu.edu>
2023-12-13 16:49:54 -0800

    strimilt work- update code

--- dataProcessing/mainApp.py
+++ dataProcessing/mainApp.py
@@ -50,7 +50,9 @@ st.write(os.path.realpath(__file__))
 st.write(os.path.dirname(os.path.realpath(__file__)))
 st.write(Path.cwd())
 # st.write(Path.iterdir("."))
-st.write(os.listdir('/mount/src'))
+from pathlib import Path
+st.write(Path(".").iterdir())
+# st.write(os.listdir('/mount/src'))
 
 with st.form(key='image_form'):
     uploaded_file = st.file_uploader("**Choose an Image**")
